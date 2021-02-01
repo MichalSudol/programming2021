b63dbcff125937d68c1185322d092dda53422a9c
Mike_Sudo <G00398852@gmit.ie>
2021-02-01 22:12:13 +0000

    Body Mass Index (BMI)

---
+++ BMI_Calc.py
@@ -0,0 +1,21 @@
+#BMI Calculator
+#Author Michal Sudol = GMIT G00398852@gmit.ie
+
+def bmi():
+     a = float(w)/(float(h)*float(h))
+     return a
+    
+again = "y"
+
+while again.lower() == "y":
+    w = input("Enter your Weigth (kg)")
+    h = input("Enter your Height (m.cm)")
+
+
+    
+    print ("Your BMI is :", bmi())
+
+    again = input("Do you want to try again?y/n:")
+    if again == "n":
+        print('Enjoy the healthy Day!!');
+        break
