df2672e3150146e31e7e5d8079f8933a0e2004b6
Mike_Sudo <G00398852@gmit.ie>
2021-04-06 18:48:47 +0100

    'Week8'

---
+++ W7.py
@@ -0,0 +1,13 @@
+#SQUAREroot Calculator
+#Author Michal Sudol = GMIT G00398852@gmit.ie
+#python squareroot.py 2021
+
+
+
+
+
+
+
+
+
+
---
+++ W8.py
@@ -0,0 +1,6 @@
+#SQUAREroot Calculator
+#Author Michal Sudol = GMIT G00398852@gmit.ie
+#python squareroot.py 2021
+
+
+
